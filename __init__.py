68de671d226c2c0087d65cc9a34b4efd16baaf9f
urapadmin <38838314+urapadmin@users.noreply.github.com>
2022-02-17 13:12:00 -0500

    applied proper versions to all plugins

--- __init__.py
+++ __init__.py
@@ -13,57 +13,53 @@ if "mcpcore.mcpworker" not in sys.modules:
     from .logviewercontroller import register_resources
     from authorization import ENTER_ADMINISTRATION_PRIVILEGE
 
-plugin: Union[KioskControllerPlugin, None] = None
+    plugin: Union[KioskControllerPlugin, None] = None
 
 
-def get_plugin_version():
-    return plugin_version
+    def instantiate_plugin_object(name, package):
+        return KioskControllerPlugin(name, package, plugin_version=plugin_version)
 
 
-def instantiate_plugin_object(name, package):
-    return KioskControllerPlugin(name, package)
+    def init_app(app, api=None):
+        app.register_blueprint(logviewer)
+
+        if api:
+            register_resources(api)
+            return True
+        else:
+            logging.error("logviewerplugin/__init__.py/init_app: api is None.")
+            return False
 
 
-def init_app(app, api=None):
-    app.register_blueprint(logviewer)
-
-    if api:
-        register_resources(api)
-        return True
-    else:
-        logging.error("logviewerplugin/__init__.py/init_app: api is None.")
-        return False
+    def register_plugin_instance(plugin_to_register):
+        global plugin
+        plugin = plugin_to_register
 
 
-def register_plugin_instance(plugin_to_register):
-    global plugin
-    plugin = plugin_to_register
+    def all_plugins_ready():
+        global plugin
+        plugin._app.events.subscribe("administration", "show_log", administration_show_log)
+        logging.debug(f"PluginLocusRelationsHook subscribed to synchronization.after_synchronization")
 
 
-def all_plugins_ready():
-    global plugin
-    plugin._app.events.subscribe("administration", "show_log", administration_show_log)
-    logging.debug(f"PluginLocusRelationsHook subscribed to synchronization.after_synchronization")
+    def administration_show_log(filename):
+        return url_for("logviewer.logviewer_show", filename=filename)
 
 
-def administration_show_log(filename):
-    return url_for("logviewer.logviewer_show", filename=filename)
+    def register_menus():
+        """
+        registeres the menu items to be shown for this plugin.
+        :return: an array of KioskMenuItem instances
+        """
+        global plugin
+        return []
 
 
-def register_menus():
-    """
-    registeres the menu items to be shown for this plugin.
-    :return: an array of KioskMenuItem instances
-    """
-    global plugin
-    return []
+    def register_global_routes():
+        global plugin
+        return []
 
 
-def register_global_routes():
-    global plugin
-    return []
-
-
-def register_global_scripts():
-    # return {"fileimportplugin": ["fileimport.static", "scripts/fileimport.js", "async"]}
-    return {}
+    def register_global_scripts():
+        # return {"fileimportplugin": ["fileimport.static", "scripts/fileimport.js", "async"]}
+        return {}
